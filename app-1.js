f3b43efea3969523aaae19d4bef71b3ebe83e041
nicolasrodri18 <pufiavila@gmail.com>
2025-11-19 16:02:13 -0500

    Terminacion del ejercicio 1

---
+++ app-1.js
@@ -0,0 +1,16 @@
+const frutas = ["Manzana", "Pera", "Banano"];
+// • Realice las siguientes operaciones y escriba el resultado final del arreglo:
+// • Agregue "Mango" al final.
+frutas.push("Mango");
+console.log(frutas);
+// • Elimine el último elemento.
+frutas.pop();
+console.log(frutas);
+// • Agregue "Uva" al inicio.
+frutas.unshift("Uva");
+console.log(frutas);
+// • Elimine el primer elemento.
+frutas.shift();
+console.log(frutas);
+// • Escriba cuántos elementos tiene el arreglo actualmente usando length.
+console.log(frutas.length);
